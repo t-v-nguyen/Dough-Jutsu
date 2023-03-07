6294c82ed167bf62da4aaee57b70f1c50af81658
Tony <35637486+t-v-nguyen@users.noreply.github.com>
2023-03-06 23:32:30 -0500

    Created Menus and Scenes

---
+++ DoughJutsu/Assets/_Scripts/Menus.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menus : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("ModeMenu");
+    }
+
+    public void Leaderboard()
+    {
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Kitchen()
+    {
+        SceneManager.LoadScene("KitchenMenu");
+    }
+
+    public void Raid()
+    {
+        SceneManager.LoadScene("RaidMenu");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void House()
+    {
+        SceneManager.LoadScene("HouseStage");
+    }
+}
